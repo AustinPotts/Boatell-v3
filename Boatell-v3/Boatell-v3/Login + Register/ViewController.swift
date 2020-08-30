9f2a4f31720d8c2e8f1b547d42c0b8336e027444
AustinPotts <pottsevolvements@gmail.com>
2020-08-30 14:53:54 -0400

    Configured backend logic for User Sign In

--- Boatell-v3/Boatell-v3/Login + Register/ViewController.swift
+++ Boatell-v3/Boatell-v3/Login + Register/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
@@ -26,9 +27,44 @@ class ViewController: UIViewController {
         textBoxView.layer.cornerRadius = 35
         loginButton.layer.cornerRadius = 30
     }
+    
+    //MARK: - Set Up User Log In
+       func handleLogIn() {
+            
+            guard let email = username.text, let password = password.text else { return }
+            
+            Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+                if let error = error {
+                    
+                 //  self.showAlert() // Show Wrong Email or Password Alert
+                    print("Error signing in: \(error)")
+                    return
+                }
+                
+                self.performSegue(withIdentifier: "LogInSegue", sender: self)
+            }
+            
+        }
+    
+    //MARK: - Set Up Login Animation
+      func animateLogin() {
+          UIView.animate(withDuration: 0.2, animations: {               //45 degree rotation. USE RADIANS
+              self.loginButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi / 0.1).concatenating(CGAffineTransform(scaleX: 0.8, y: 0.8))
+                  
+              }) { (_) in //Is finished
+                  
+                  
+                  UIView.animate(withDuration: 0.01, animations: {
+                      self.loginButton.transform = .identity
+                  })
+                                  
+              }
+      }
 
     
     @IBAction func loginButtonTapped(_ sender: Any) {
+        handleLogIn()
+        animateLogin()
     }
     
 
